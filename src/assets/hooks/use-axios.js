0f5d40d399944454bc1136b59b92984c2d4d17c6
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-08-14 10:12:36 -0300

    fix: destructuring assignment

--- src/assets/hooks/use-axios.js
+++ src/assets/hooks/use-axios.js
@@ -2,7 +2,7 @@ import restClient from "../helper/rest_client_axios";
 import { useState, useEffect } from "react";
 
 export default function useAxios(configParams) {
-    const [method, url, params = {}, data = {}] = configParams;
+    const {method, url, params = {}, data = {}} = configParams;
     const [useData, setData] = useState([])
     const [useError, setError] = useState(null)
     const [isLoader, setIsLoader] = useState(true)
