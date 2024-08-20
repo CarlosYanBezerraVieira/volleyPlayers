e5e5667b9a7458982364cbf81f771effdda29038
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2024-08-20 16:54:45 -0300

    refactor: change use axios

--- src/hooks/use-axios.js
+++ src/hooks/use-axios.js
@@ -4,24 +4,28 @@ import { useState } from "react";
 export default function useAxios(configParams) {
 
     const { method, url, params = {}, data = {} } = configParams;
-    const [useData, setData] = useState([])
-    const [useError, setError] = useState('')
-    const [isLoader, setIsLoader] = useState(true)
-
+    const [useData, setData] = useState([]);
+    const [useError, setError] = useState('');
+    const [isLoader, setIsLoader] = useState(true);
 
     const fetchData = async () => {
         try {
-         
-            let response = await restClient[method.toLowerCase()](url, JSON.stringify({ ...params, ...data }))
-            setData(response.data)
-         
+            let response;
+            
+            if (method.toLowerCase() === 'get') {
+                response = await restClient.get(url, { params });
+            } else {
+                response = await restClient[method.toLowerCase()](url, data, { params });
+            }
+
+            setData(response.data);
 
         } catch (error) {
-            setError(error)
+            setError(error.message || 'An error occurred');
         } finally {
-            setIsLoader(false)
+            setIsLoader(false);
         }
     }
-    return { useData, useError, isLoader, fetchData }
 
+    return { useData, useError, isLoader, fetchData };
 }
